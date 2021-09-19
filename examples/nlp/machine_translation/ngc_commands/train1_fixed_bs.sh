e071c9381c1c2c2b76238c0e20bec6e55d0cdcbc
PeganovAnton <peganoff2@mail.ru>
2021-09-19 21:41:11 +0300

    Add ngc config and command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/ngc_commands/train1_fixed_bs.sh
@@ -0,0 +1,25 @@
+WANDB_API_KEY="$1"
+
+read -r -d '' command << EOF
+set -e -x
+git clone https://github.com/NVIDIA/NeMo
+cd NeMo
+git checkout iwslt_cascade
+pip install -r requirements/requirements.txt
+pip install -r requirements/requirements_lightning.txt
+pip install -r requirements/requirements_test.txt
+pip install -r requirements/requirements_nlp.txt
+export PYTHONPATH="\$(pwd)"
+cd examples/nlp/machine_translation
+wandb login ${WANDB_API_KEY}
+python enc_dec_nmt.py --config-path=conf --config-name aayn_base_punc_fixed_bs_ngc trainer.gpus=1
+set +e +x
+EOF
+
+ngc batch run \
+  --instance dgx1v.16g.1.norm \
+  --name "ml-model.aayn fixed_beam_search_autoreg_punc_cap_mt" \
+  --image "nvidia/pytorch:21.08-py3" \
+  --result /result \
+  --datasetid 88290:/data \
+  --commandline "${command}"
