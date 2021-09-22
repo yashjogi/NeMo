08b74403782ce73c111375c7e40e19f20e029939
PeganovAnton <peganoff2@mail.ru>
2021-09-22 13:52:18 +0300

    Add inference command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/punctuation_infer/free_beam_search.sh
@@ -0,0 +1,8 @@
+ds_path=~/NeMo/examples/speech_translation/prepared_punctuation_data_all_punctuation
+python nmt_transformer_infer.py \
+    --model=~/NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_max_punc/2308718/nemo_experiments/AAYNBase/2021-09-20_09-43-54/checkpoints/AAYNBase.nemo \
+    --srctext="${ds_path}/test/input.txt" \
+    --tgtout="${ds_path}/predictions/aayn_base_free_beam_search_job2308718/pred.txt" \
+    --target_lang en \
+    --source_lang en \
+
