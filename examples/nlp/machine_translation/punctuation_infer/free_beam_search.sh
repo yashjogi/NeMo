6ca1824a679e3f3e60263a824c0f7122f2df07ac
PeganovAnton <peganoff2@mail.ru>
2021-09-22 13:59:39 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/free_beam_search.sh
+++ examples/nlp/machine_translation/punctuation_infer/free_beam_search.sh
@@ -1,8 +1,10 @@
 ds_path=/home/lab/NeMo/examples/speech_translation/prepared_punctuation_data_all_punctuation
+output="${ds_path}/predictions/aayn_base_free_beam_search_job2308718/pred.txt"
+mkdir -p "$(dirname "${output}")"
 python nmt_transformer_infer.py \
     --model=/home/lab/NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_max_punc/2308718/nemo_experiments/AAYNBase/2021-09-20_09-43-54/checkpoints/AAYNBase.nemo \
     --srctext="${ds_path}/test/input.txt" \
-    --tgtout="${ds_path}/predictions/aayn_base_free_beam_search_job2308718/pred.txt" \
+    --tgtout="${output}" \
     --target_lang en \
     --source_lang en \
 
