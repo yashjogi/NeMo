07fffeb1c90ee9ef735a0fe8b1c4181955f46163
PeganovAnton <peganoff2@mail.ru>
2021-09-26 23:23:14 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/debug_prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
+++ examples/speech_translation/debug_prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
@@ -1,7 +1,7 @@
 python prepare_wmt_data_for_punctuation_capitalization_task.py \
   ~/data/TED_Talks/en-ja/train.tags.en-ja.en \
   --input_language en \
-  --output_dir prepared_punctuation_data_min_punctuation_26.9.2021_19.00 \
+  --output_dir prepared_punctuation_data_min_punctuation_26.9.2021_23.23 \
   --corpus_types TED \
   --test_ratio 0.05 \
   --clean_data_dir cleaned_wmt_min_punc \
