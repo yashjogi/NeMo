206790d8f9cacb8e14bf1715d215f1755f1dfe7d
PeganovAnton <peganoff2@mail.ru>
2021-09-26 22:25:56 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
+++ examples/speech_translation/prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
@@ -1,8 +1,8 @@
 python prepare_wmt_data_for_punctuation_capitalization_task.py \
-  europarl/v10/training-monolingual/europarl-v10.en.tsv \
-  news-commentary/v16/training-monolingual/news-commentary-v16.en \
-  rapid/RAPID_2019.de-en.xlf \
-  TED_Talks/en-ja/train.tags.en-ja.en \
+  ~/data/europarl/v10/training-monolingual/europarl-v10.en.tsv \
+  ~/data/news-commentary/v16/training-monolingual/news-commentary-v16.en \
+  ~/data/rapid/RAPID_2019.de-en.xlf \
+  ~/data/TED_Talks/en-ja/train.tags.en-ja.en \
   --input_language en \
   --output_dir prepared_punctuation_data_min_punctuation_26.9.2021_19.00 \
   --corpus_types europarl 'news-commentary' rapid TED \
