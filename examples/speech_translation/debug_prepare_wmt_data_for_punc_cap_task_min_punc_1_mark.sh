95c66f06a2a993b53c2b0d623447fb7f20771ef6
PeganovAnton <peganoff2@mail.ru>
2021-09-26 22:26:50 +0300

    Add script for dataset creation
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/debug_prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
@@ -0,0 +1,11 @@
+python prepare_wmt_data_for_punctuation_capitalization_task.py \
+  ~/data/TED_Talks/en-ja/train.tags.en-ja.en \
+  --input_language en \
+  --output_dir prepared_punctuation_data_min_punctuation_26.9.2021_19.00 \
+  --corpus_types TED \
+  --clean_data_dir cleaned_wmt_min_punc \
+  --create_model_input \
+  --autoregressive_labels \
+  --bert_labels \
+  --allowed_punctuation ',.?' \
+  --only_first_punctuation_character_after_word_in_autoregressive
