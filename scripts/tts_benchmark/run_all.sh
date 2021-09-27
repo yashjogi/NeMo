aef6deab8c103ef999ea468c57e5ce7a48e24f31
Stanislav Beliaev <stasbelyaev96@gmail.com>
2021-09-27 17:55:15 +0300

    OOM nonzero exit code => NaN fix.

--- scripts/tts_benchmark/run_all.sh
+++ scripts/tts_benchmark/run_all.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-MODELS="mixer-tts-1 mixer-tts-2 mixer-tts-3 fastpitch"
-BATCH_SIZES="1 2 4 8 16 32 64 128 256 512 1024"
+#MODELS="mixer-tts-1 mixer-tts-2 mixer-tts-3 fastpitch"
+MODELS="fastpitch"
+#BATCH_SIZES="1 2 4 8 16 32 64 128 256 512 1024"
+BATCH_SIZES="1024"
 #N_CHARSS="64 128 256 512 1024 2048 4096"
-N_CHARSS="128"
+N_CHARSS="4096"
 OUTPUT=${1:-"output.csv"}
 
 # Header
@@ -17,8 +19,15 @@ for BATCH_SIZE in $BATCH_SIZES ; do
   for N_CHARS in $N_CHARSS ; do
     result_csv="$result_csv$BATCH_SIZE,$N_CHARS"
     for MODEL in $MODELS ; do
+      set -o pipefail
       rtf=$(BATCH_SIZE=$BATCH_SIZE N_CHARS=$N_CHARS scripts/tts_benchmark/run.sh "$MODEL" | grep avg_RTF | cut -c 10-)
-      result_csv="$result_csv,$rtf"
+      # shellcheck disable=SC2181
+      if [ $? -ne 0 ];
+      then
+        result_csv="$result_csv,NaN"
+      else
+        result_csv="$result_csv,$rtf"
+      fi
     done
     result_csv="$result_csv\n"
   done
