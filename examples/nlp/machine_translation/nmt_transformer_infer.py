e5896c270479c461f3675a5c37222473efafbddc
PeganovAnton <peganoff2@mail.ru>
2021-09-29 10:03:54 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/nmt_transformer_infer.py
+++ examples/nlp/machine_translation/nmt_transformer_infer.py
@@ -154,13 +154,6 @@ def main():
                 max_delta_length=args.max_delta_length,
             )
         else:
-            if args.word_tokens is not None:
-                model.decoder_tokenizer.word_ids = model.decoder_tokenizer.tokens_to_ids(args.word_tokens)
-                test_cfg = model._cfg.test_ds
-                test_cfg.src_file_name = "/home/lab/NeMo/examples/speech_translation/prepared_punctuation_data_all_punctuation/test/input.txt"
-                test_cfg.tgt_file_name = "/home/lab/NeMo/examples/speech_translation/prepared_punctuation_data_all_punctuation/test/autoregressive_labels.txt"
-                test_cfg['add_src_num_words_to_batch'] = args.add_src_num_words_to_batch
-                model.setup_test_data(test_cfg)
             model.beam_search = BeamSearchSequenceGenerator(
                 embedding=model.decoder.embedding,
                 decoder=model.decoder.decoder,
