d13449d69558d917eaccca408d3ce43e91e1111c
PeganovAnton <peganoff2@mail.ru>
2021-09-29 09:48:19 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/fixed_beam_search_min_punc_autoregressive.sh
+++ examples/nlp/machine_translation/punctuation_infer/fixed_beam_search_min_punc_autoregressive.sh
@@ -1,8 +1,8 @@
-ds_path=/home/lab/NeMo/examples/speech_translation/prepared_punctuation_data_all_punctuation
+ds_path=/home/apeganov/NeMo/examples/speech_translation/prepared_punctuation_data_all_punctuation
 output="${ds_path}/predictions/test_min_punc/free_beam_search_gpu1_steps300k_lr1e-4_bs16384_2320685/pred.txt"
 mkdir -p "$(dirname "${output}")"
 python nmt_transformer_infer.py \
-    --model=/home/lab/NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_min_punc_autoregressivefree_beam_search_gpu1_steps300k_lr1e-4_bs16384_2320685/nemo_experiments/AAYNBase/2021-09-26_22-57-10/checkpoints/AAYNBase.nemo \
+    --model=/home/apeganov/NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_min_punc_autoregressivefree_beam_search_gpu1_steps300k_lr1e-4_bs16384_2320685/nemo_experiments/AAYNBase/2021-09-26_22-57-10/checkpoints/AAYNBase.nemo \
     --srctext="${ds_path}/test_min_punc/input.txt" \
     --tgtout="${output}" \
     --target_lang en \
