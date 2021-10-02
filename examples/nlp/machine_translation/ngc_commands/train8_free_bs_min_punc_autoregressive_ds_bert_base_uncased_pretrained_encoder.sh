186727c79a05b6bb0eb13e05020b10f008f2e0aa
PeganovAnton <peganoff2@mail.ru>
2021-10-02 08:57:15 +0300

    Reduce batch size
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/ngc_commands/train8_free_bs_min_punc_autoregressive_ds_bert_base_uncased_pretrained_encoder.sh
+++ examples/nlp/machine_translation/ngc_commands/train8_free_bs_min_punc_autoregressive_ds_bert_base_uncased_pretrained_encoder.sh
@@ -16,7 +16,10 @@ cd examples/nlp/machine_translation
 wandb login ${WANDB_API_KEY}
 python enc_dec_nmt.py --config-path=conf \
   --config-name aayn_base_min_punc_autoregressive_ds_bert_base_uncased_pretrained_encoder_ngc \
-  trainer.gpus=8
+  trainer.gpus=8 \
+  model.train_ds.tokens_in_batch=9196 \
+  model.validation_ds.tokens_in_batch=9196 \
+  model.test_ds.tokens_in_batch=9196
 set +e +x
 EOF
 
