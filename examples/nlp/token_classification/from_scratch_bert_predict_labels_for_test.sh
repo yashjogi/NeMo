ccc12377ca8a085bce7eb6cdb32640dc3c2b1152
PeganovAnton <peganoff2@mail.ru>
2021-10-03 16:27:30 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/from_scratch_bert_predict_labels_for_test.sh
+++ examples/nlp/token_classification/from_scratch_bert_predict_labels_for_test.sh
@@ -1,7 +1,7 @@
 python punctuate_capitalize_infer.py \
   --input_text ~/NeMo/examples/nlp/token_classification/data/wmt/test/text_test.txt \
   --output_text ~/NeMo/examples/nlp/token_classification/data/predictions_wmt/2329633_evelina_model_from_scratch/pred.txt \
-  --model_path '~/NeMo/examples/nlp/token_classification/ngc_results/wmt/min_punc/2329633_lr1e-4_steps300000_gpu1_from_scratch/final.nemo' \
+  --model_path '~/NeMo/examples/nlp/token_classification/ngc_results/wmt/min_punc/2329633_lr1e-4_steps300000_gpu1_from_scratch/nemo_experiments/Punctuation_and_Capitalization/2021-10-01_21-12-55/checkpoints/Punctuation_and_Capitalization.nemo' \
   --max_seq_length 512 \
   --step 510 \
   --margin 0 \
