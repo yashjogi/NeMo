5c665f9c47204c05bbbb091aa80df045d2e951f4
PeganovAnton <peganoff2@mail.ru>
2021-10-03 15:34:05 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/pretrained_bert_base_gpu1_min_punc_autoregressive.sh
+++ examples/nlp/machine_translation/punctuation_infer/pretrained_bert_base_gpu1_min_punc_autoregressive.sh
@@ -1,10 +1,10 @@
 ds_path=/home/apeganov/NeMo/examples/nlp/token_classification/data
 job_results_name="2329536_pretrained_hf_bert_base_uncased_steps300000_gpu1_bs10240_lr1e-4"
 output="${ds_path}/predictions_wmt/test_min_punc/${job_results_name}/pred.txt"
-model="~/NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_min_punc_autoregressive/${job_results_name}/nemo_experiments/AAYNBase/best.nemo"
+model="/home/apeganov/NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_min_punc_autoregressive/${job_results_name}/nemo_experiments/AAYNBase/best.nemo"
 mkdir -p "$(dirname "${output}")"
 python nmt_transformer_infer.py \
-    --model="$(realpath "${model}")" \
+    --model="${model}" \
     --srctext="${ds_path}/wmt/test/text_test.txt" \
     --tgtout="${output}" \
     --target_lang en \
