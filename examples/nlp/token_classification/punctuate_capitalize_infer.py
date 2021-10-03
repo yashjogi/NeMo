04db75c7476083ae80bea42e890cce0b76113b75
PeganovAnton <peganoff2@mail.ru>
2021-10-03 16:39:35 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/punctuate_capitalize_infer.py
+++ examples/nlp/token_classification/punctuate_capitalize_infer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import sys
-sys.path = ["/home/lab/NeMo"] + sys.path
+sys.path = ["/home/apeganov/NeMo"] + sys.path
 
 import argparse
 import json
