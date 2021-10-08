d132a3842f2f72bfc9e96e0d6bcc042c81c0556b
PeganovAnton <peganoff2@mail.ru>
2021-10-08 09:07:56 +0300

    Minor fixes
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
+++ examples/speech_translation/punct_ds_preparation/prepare_wmt_data_for_punc_cap_task_min_punc_1_mark.sh
@@ -4,13 +4,13 @@ python prepare_wmt_data_for_punctuation_capitalization_task.py \
   ~/data/rapid/RAPID_2019.de-en.xlf \
   ~/data/TED_Talks/en-ja/train.tags.en-ja.en \
   --input_language en \
-  --output_dir prepared_punctuation_data_min_punctuation_27.9.2021_24.01 \
+  --output_dir punct_prepared_datasets/prepared_punctuation_data_min_punctuation_27.9.2021_24.01 \
   --corpus_types europarl 'news-commentary' rapid TED \
-  --test_ratio 0.05 \
+  --test_size 2000 \
   --clean_data_dir cleaned_wmt_min_punc \
   --create_model_input \
   --autoregressive_labels \
   --bert_labels \
   --allowed_punctuation ',.?' \
   --only_first_punctuation_character_after_word_in_autoregressive \
-  --no_label_for_all_characters_are_upper_case
+  --no_label_if_all_characters_are_upper_case
