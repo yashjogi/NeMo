a26242d152b793d574067702052e911097df2e59
PeganovAnton <peganoff2@mail.ru>
2021-10-12 17:08:56 +0300

    Add example
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/conf/speedup/ngc_commands/no_wandb.sh
+++ examples/nlp/machine_translation/conf/speedup/ngc_commands/no_wandb.sh
@@ -1,6 +1,7 @@
+WANDB_API_KEY="$1"
+
 read -r -d '' command << EOF
 set -e -x
-export OMP_NUM_THREADS=8
 mkdir /result/nemo_experiments
 git clone https://github.com/NVIDIA/NeMo
 cd NeMo
@@ -11,18 +12,14 @@ pip install -r requirements/requirements_test.txt
 pip install -r requirements/requirements_nlp.txt
 export PYTHONPATH="\$(pwd)"
 cd examples/nlp/machine_translation
-python enc_dec_nmt.py \
-  --config-path=conf/speedup \
-  --config-name original \
-  trainer.gpus=1 \
-  exp_manager.create_wandb_logger=false
-
+wandb login ${WANDB_API_KEY}
+python enc_dec_nmt.py --config-path=conf/speedup --config-name original trainer.gpus=1
 set +e +x
 EOF
 
 ngc batch run \
   --instance dgx1v.16g.1.norm \
-  --name "ml-model.aayn example_punctuation_and_capitalization" \
+  --name "ml-model.aayn speedup_original" \
   --image "nvidia/pytorch:21.08-py3" \
   --result /result \
   --datasetid 88728:/data \
