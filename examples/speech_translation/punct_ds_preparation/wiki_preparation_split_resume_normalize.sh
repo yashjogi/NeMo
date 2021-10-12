a80cd14b6c4089a6685977e8d5f814cc3abec080
PeganovAnton <peganoff2@mail.ru>
2021-10-12 21:37:38 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/wiki_preparation_split_resume_normalize.sh
+++ examples/speech_translation/punct_ds_preparation/wiki_preparation_split_resume_normalize.sh
@@ -10,5 +10,5 @@ python prepare_big_data_for_punctuation_capitalization_task.py \
   --only_first_punctuation_character_after_word_in_autoregressive \
   --no_label_if_all_characters_are_upper_case \
   --input_files ~/data/enwiki-20210920-pages-articles-multistream.xml \
-  --resume_from_normalization \
+  --resume_from normalization \
   -n
