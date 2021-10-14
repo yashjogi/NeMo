5a96309db5db1adf6b006004096d8acd4d9b5c8d
PeganovAnton <peganoff2@mail.ru>
2021-10-14 10:05:00 +0300

    Remove leading new line
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/prepare_big_data_for_punctuation_capitalization_task.py
+++ examples/speech_translation/punct_ds_preparation/prepare_big_data_for_punctuation_capitalization_task.py
@@ -431,6 +431,8 @@ def get_wiki_text_lines(text, lang, tokenizer, tok_chars, untok_chars, pos_info,
     text = text.replace("''", '"')
     text = DOUBLE_SQUARE_BRACKETS_WITH_CONTENT.sub(double_square_brackets_replacement, text)
     text = NEW_LINE_DUP.sub('\n', text)
+    if text[0] == '\n':
+        text = text[1:]
     # text = remove_lists(text)
     text = text.replace('[', '(')
     text = text.replace(']', ')')
