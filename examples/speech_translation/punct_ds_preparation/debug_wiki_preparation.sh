652dca73dfd3ee4da00113fccdb319a7b811eb22
PeganovAnton <peganoff2@mail.ru>
2021-10-14 09:50:41 +0300

    Update debug script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/debug_wiki_preparation.sh
+++ examples/speech_translation/punct_ds_preparation/debug_wiki_preparation.sh
@@ -10,5 +10,4 @@ python prepare_big_data_for_punctuation_capitalization_task.py \
   --only_first_punctuation_character_after_word_in_autoregressive \
   --no_label_if_all_characters_are_upper_case \
   --input_files ~/data/small_enwiki.txt \
-  -n \
-  --resume_from normalization
+  -n
