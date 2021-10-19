62240fb76af9ce01c8db8590e6c95b8902d88631
PeganovAnton <peganoff2@mail.ru>
2021-10-19 04:14:38 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/debug_wiki_preparation.sh
+++ examples/speech_translation/punct_ds_preparation/debug_wiki_preparation.sh
@@ -8,5 +8,5 @@ python prepare_big_data_for_punctuation_capitalization_task_simple.py \
   --only_first_punctuation_character_after_word_in_autoregressive \
   --no_label_if_all_characters_are_upper_case \
   --input_files ~/data/small_enwiki.txt \
-  --num_jobs 24 #\
-  #--resume_from cutting
+  --num_jobs 24 \
+  --resume_from cutting
