c3f34d726c1e59a486d534e5ba3bd9d26c0cfffe
PeganovAnton <peganoff2@mail.ru>
2021-10-19 22:32:44 +0300

    Add scripts for training large
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/token_classification/ngc_commands/train8_large_on_wmt.sh
@@ -0,0 +1,27 @@
+WANDB_API_KEY="$1"
+
+read -r -d '' command << EOF
+set -e -x
+OMP_NUM_THREADS=1
+git clone https://github.com/NVIDIA/NeMo
+mkdir -p /result/nemo_experiments
+cd NeMo
+git checkout iwslt_cascade
+source reinstall.sh
+cd examples/nlp/token_classification
+wandb login ${WANDB_API_KEY}
+python -c "from nemo.collections.nlp.modules import get_tokenizer;get_tokenizer('bert-large-uncased', use_fast=False)"
+python punctuation_capitalization_train.py --config-path=conf \
+    --config-name wmt_train \
+    trainer.gpus=8 \
+    model.language_model.pretrained_model_name=bert-large-uncased
+set +e +x
+EOF
+
+ngc batch run \
+  --instance dgx1v.16g.1.norm \
+  --name "ml-model.bert large_punctuation_capitalization_training_on_wmt" \
+  --image "nvidia/pytorch:21.08-py3" \
+  --result /result \
+  --datasetid 88512:/data \
+  --commandline "${command}"
---
+++ examples/nlp/token_classification/ngc_commands/train8_large_on_wmt_sleep.sh
@@ -0,0 +1,24 @@
+WANDB_API_KEY="$1"
+
+read -r -d '' command << EOF
+set -e -x
+OMP_NUM_THREADS=1
+git clone https://github.com/NVIDIA/NeMo
+mkdir -p /result/nemo_experiments
+cd NeMo
+git checkout iwslt_cascade
+source reinstall.sh
+cd examples/nlp/token_classification
+wandb login ${WANDB_API_KEY}
+python -c "from nemo.collections.nlp.modules import get_tokenizer;get_tokenizer('bert-large-uncased', use_fast=False)"
+sleep 300000
+set +e +x
+EOF
+
+ngc batch run \
+  --instance dgx1v.16g.1.norm \
+  --name "ml-model.bert large_punctuation_capitalization_training_on_wmt" \
+  --image "nvidia/pytorch:21.08-py3" \
+  --result /result \
+  --datasetid 88512:/data \
+  --commandline "${command}"
