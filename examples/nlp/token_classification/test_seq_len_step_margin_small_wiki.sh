4614e5052f5463d2da2be97bb8b799044f119f84
PeganovAnton <peganoff2@mail.ru>
2021-10-22 14:42:45 +0300

    fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/test_seq_len_step_margin_small_wiki.sh
+++ examples/nlp/token_classification/test_seq_len_step_margin_small_wiki.sh
@@ -3,4 +3,6 @@ python test_seq_len_step_margin.py \
   --labels ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/labels_iwslt_en_text.txt \
   --source_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
   --output_dir scores_small_wiki_posttrained_job2358708 \
-
+  --max_seq_length 32 48 64 92 128 \
+  --margin 8 12 16 \
+  --step 4 8 14
