3b0d69d5af5a5813e075d7b24bb552b70e8db608
PeganovAnton <peganoff2@mail.ru>
2021-10-22 14:41:43 +0300

    Add script for scoring small wiki ckpt
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/token_classification/test_seq_len_step_margin_small_wiki.sh
@@ -0,0 +1,6 @@
+python test_seq_len_step_margin.py \
+  --model_path '~/NeMo/examples/nlp/token_classification/ngc_results/small_wiki/min_punc/2358708_lr1e-4_steps400000_gpu1_unsorted/nemo_experiments/Punctuation_and_Capitalization/2021-10-20_17-50-30/checkpoints/Punctuation_and_Capitalization.nemo' \
+  --labels ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/labels_iwslt_en_text.txt \
+  --source_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
+  --output_dir scores_small_wiki_posttrained_job2358708 \
+
