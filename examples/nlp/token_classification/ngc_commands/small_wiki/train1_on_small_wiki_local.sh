9ea4390c0a2b93d68c5e06fef7e137024b78c45f
PeganovAnton <peganoff2@mail.ru>
2021-10-22 08:57:34 +0300

    Update launch script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/ngc_commands/small_wiki/train1_on_small_wiki_local.sh
+++ examples/nlp/token_classification/ngc_commands/small_wiki/train1_on_small_wiki_local.sh
@@ -1,5 +1,5 @@
 python punctuation_capitalization_train.py \
     --config-path=conf/small_wiki \
-    --config-name small_wiki_train_local \
+    --config-name small_wiki_train_sorted_local \
     trainer.gpus=1 \
     exp_manager.create_wandb_logger=false
