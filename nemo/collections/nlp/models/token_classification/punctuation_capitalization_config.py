7327cc0967052afa6d8c17f1c22402ffeef518bb
PeganovAnton <peganoff2@mail.ru>
2021-10-25 21:30:03 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/models/token_classification/punctuation_capitalization_config.py
+++ nemo/collections/nlp/models/token_classification/punctuation_capitalization_config.py
@@ -83,8 +83,8 @@ class PunctuationCapitalizationModelConfig:
         use_tarred_dataset=MISSING,
         metadata_file=MISSING,
     )
-    punct_label_ids: type(None) = None
-    capit_label_ids: type(None) = None
+    punct_label_ids: Optional = None
+    capit_label_ids: Optional = None
     class_labels: Optional[ClassLabels] = ClassLabels()
 
     punct_head: HeadConfig = HeadConfig()
