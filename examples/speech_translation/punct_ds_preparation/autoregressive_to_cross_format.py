d1b6e9666aaf23da984a5eb7c5383dc6a41807f9
PeganovAnton <peganoff2@mail.ru>
2021-10-26 21:43:24 +0300

    Make minor improvements
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/autoregressive_to_cross_format.py
+++ examples/speech_translation/punct_ds_preparation/autoregressive_to_cross_format.py
@@ -4,6 +4,11 @@ import re
 import string
 from collections import Counter
 from pathlib import Path
+from subprocess import run, PIPE
+
+from tqdm import tqdm
+
+from nemo.utils import logging
 
 
 ENCODINGS = string.ascii_letters + string.digits
@@ -27,14 +32,16 @@ def get_args():
 
 def collect_cross_vocabulary(input_file):
     vocabulary = Counter()
+    result = run(['wc', '-l', str(input_file)], stdout=PIPE, stderr=PIPE)
+    num_lines = int(result.stdout.decode('utf-8').split()[0])
     with input_file.open() as f:
-        for line in f:
+        for line in tqdm(f, total=num_lines, unit='line', desc="Creating vocabulary"):
             vocabulary.update(CAPITALIZATION_WITH_FOLLOWING_PUNCTUATION_RE.findall(line.strip()))
-    return dict(sorted(vocabulary.items(), key=lambda x: -x[1]))
+    return dict(sorted(vocabulary.items(), key=lambda x: -x[1])), num_lines
 
 
 def autoregressive_file_to_cross_format_file(input_file, output_file, vocab_file):
-    combinations = collect_cross_vocabulary(input_file)
+    combinations, num_lines = collect_cross_vocabulary(input_file)
     if len(combinations) > len(ENCODINGS):
         raise ValueError(
             f"Too many cross labels were found in file {input_file}. Number of cross labels: {len(combinations)}, "
@@ -47,8 +54,8 @@ def autoregressive_file_to_cross_format_file(input_file, output_file, vocab_file
     with vocab_file.open('w') as f:
         json.dump(vocabulary, f)
     inverse_vocabulary = {v['string']: k for k, v in vocabulary.items()}
-    with input_file.open() as in_f, output_file.open() as out_f:
-        for line in in_f:
+    with input_file.open() as in_f, output_file.open('w') as out_f:
+        for line in tqdm(in_f, total=num_lines, unit='line', desc="Encoding"):
             for piece in CAPITALIZATION_WITH_FOLLOWING_PUNCTUATION_RE.findall(line.strip()):
                 out_f.write(inverse_vocabulary[piece])
         out_f.write('\n')
