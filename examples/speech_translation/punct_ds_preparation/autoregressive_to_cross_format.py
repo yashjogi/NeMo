eb722465b32ae3376e8eb508d062076351d025a4
PeganovAnton <peganoff2@mail.ru>
2021-10-26 23:43:46 +0300

    Add option of using ready vocabulary
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/autoregressive_to_cross_format.py
+++ examples/speech_translation/punct_ds_preparation/autoregressive_to_cross_format.py
@@ -8,8 +8,6 @@ from subprocess import run, PIPE
 
 from tqdm import tqdm
 
-from nemo.utils import logging
-
 
 ENCODINGS = string.ascii_letters + string.digits
 CAPITALIZATION_LABELS = 'uOU'
@@ -21,15 +19,25 @@ def get_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--input", "-i", type=Path, required=True)
     parser.add_argument("--output", "-o", type=Path, required=True)
-    parser.add_argument(
-        "--vocab", "-v", type=Path, required=True, help="Path to file where transition vocabulary will be saved."
+    vocab = parser.add_mutually_exclusive_group(required=True)
+    vocab.add_argument(
+        "--output_vocab", "-v", type=Path, help="Path to file where transition vocabulary will be saved."
     )
+    vocab.add_argument("--ready_vocab", "-r", type=Path)
     parser.add_argument("--not_normalized_vocabulary", "-V", type=Path)
     parser.add_argument("--normalize", "-n", action="store_true")
     args = parser.parse_args()
     args.input = args.input.expanduser()
     args.output = args.output.expanduser()
-    args.vocab = args.vocab.expanduser()
+    if args.ready_vocab is None:
+        args.output_vocab = args.output_vocab.expanduser()
+    else:
+        args.ready_vocab = args.ready_vocab.expanduser()
+    if args.output_vocab is None and (args.not_normalized_vocabulary is not None or args.normalize):
+        raise ValueError(
+            f"If parameter `--ready_vocab` is provided you should not provide parameters `--normalize` and "
+            f"`--not_normalized_vocabulary`."
+        )
     if args.not_normalized_vocabulary is not None:
         args.not_normalized_vocabulary = args.not_normalized_vocabulary.expanduser()
     return args
@@ -39,11 +47,15 @@ def normalize(line):
     return SPACE_BEFORE_PUNCTUATION.sub(r'\1 ', line)
 
 
+def get_num_lines(input_file):
+    result = run(['wc', '-l', str(input_file)], stdout=PIPE, stderr=PIPE)
+    return int(result.stdout.decode('utf-8').split()[0])
+
+
 def collect_cross_vocabulary(input_file):
     usual_vocabulary = Counter()
     normalized_vocabulary = Counter()
-    result = run(['wc', '-l', str(input_file)], stdout=PIPE, stderr=PIPE)
-    num_lines = int(result.stdout.decode('utf-8').split()[0])
+    num_lines = get_num_lines(input_file)
     with input_file.open() as f:
         for line in tqdm(f, total=num_lines, unit='line', desc="Creating vocabulary"):
             usual_vocabulary.update(CAPITALIZATION_WITH_FOLLOWING_PUNCTUATION_RE.findall(line.strip() + ' '))
@@ -55,43 +67,56 @@ def collect_cross_vocabulary(input_file):
     return usual_vocabulary, normalized_vocabulary, num_lines
 
 
-def autoregressive_file_to_cross_format_file(
-        input_file, output_file, vocab_file, normalize_vocabulary, not_normalized_vocab_file
-):
-    combinations, normalized_combinations, num_lines = collect_cross_vocabulary(input_file)
-    if len(combinations) > len(ENCODINGS):
-        raise ValueError(
-            f"Too many cross labels were found in file {input_file}. Number of cross labels: {len(combinations)}, "
-            f"number of available encodings: {len(ENCODINGS)}. You probably need to add more characters to parameter "
-            f"`ENCODINGS` if you wish to process file {input_file}."
-        )
-    vocabulary = {}
-    for i, (s, ctr) in enumerate(combinations.items()):
-        vocabulary[ENCODINGS[i]] = {'string': s, "count": ctr}
-    normalized_vocabulary = {}
-    for i, (s, ctr) in enumerate(normalized_combinations.items()):
-        normalized_vocabulary[ENCODINGS[i]] = {'string': s, "count": ctr}
-    with vocab_file.open('w') as f:
-        json.dump(normalized_vocabulary if normalize_vocabulary else vocabulary, f, indent=2)
-    if normalized_vocabulary and not_normalized_vocab_file is not None:
-        with not_normalized_vocab_file.open('w') as f:
-            json.dump(vocabulary, f, indent=2)
-    inverse_vocabulary = {
-        v['string']: k for k, v in (normalized_vocabulary.items() if normalize_vocabulary else vocabulary.items())
-    }
+def encode(input_file, output_file, num_lines, inverse_vocabulary, normalize_labels):
     with input_file.open() as in_f, output_file.open('w') as out_f:
         for line in tqdm(in_f, total=num_lines, unit='line', desc="Encoding"):
-            if normalized_vocabulary:
+            if normalize_labels:
                 line = normalize(line)
             for piece in CAPITALIZATION_WITH_FOLLOWING_PUNCTUATION_RE.findall(line.strip() + ' '):
-                out_f.write(inverse_vocabulary[piece])
+                if piece in inverse_vocabulary:
+                    out_f.write(inverse_vocabulary[piece])
+                else:
+                    raise ValueError(f"String '{piece}' is not found in vocabulary.")
             out_f.write('\n')
+
+
+def autoregressive_file_to_cross_format_file(
+    input_file, output_file, ready_vocab_file, output_vocab_file, normalize_labels, not_normalized_vocab_file
+):
+    if ready_vocab_file is None:
+        combinations, normalized_combinations, num_lines = collect_cross_vocabulary(input_file)
+        if len(combinations) > len(ENCODINGS):
+            raise ValueError(
+                f"Too many cross labels were found in file {input_file}. Number of cross labels: {len(combinations)}, "
+                f"number of available encodings: {len(ENCODINGS)}. You probably need to add more characters to "
+                f"parameter `ENCODINGS` if you wish to process file {input_file}."
+            )
+        vocabulary = {}
+        for i, (s, ctr) in enumerate(combinations.items()):
+            vocabulary[ENCODINGS[i]] = {'string': s, "count": ctr}
+        normalized_vocabulary = {}
+        for i, (s, ctr) in enumerate(normalized_combinations.items()):
+            normalized_vocabulary[ENCODINGS[i]] = {'string': s, "count": ctr}
+        with output_vocab_file.open('w') as f:
+            json.dump(normalized_vocabulary if normalize_labels else vocabulary, f, indent=2)
+        if normalized_vocabulary and not_normalized_vocab_file is not None:
+            with not_normalized_vocab_file.open('w') as f:
+                json.dump(vocabulary, f, indent=2)
+        inverse_vocabulary = {
+            v['string']: k for k, v in (normalized_vocabulary.items() if normalize_labels else vocabulary.items())
+        }
+    else:
+        with ready_vocab_file.open() as f:
+            ready_vocabulary = json.load(f)
+        inverse_vocabulary = {v['string']: k for k, v in ready_vocabulary.items()}
+        num_lines = get_num_lines(input_file)
+    encode(input_file, output_file, num_lines, inverse_vocabulary, normalize_labels)
 
 
 def main():
     args = get_args()
     autoregressive_file_to_cross_format_file(
-        args.input, args.output, args.vocab, args.normalize, args.not_normalized_vocabulary
+        args.input, args.output, args.ready_vocab, args.output_vocab, args.normalize, args.not_normalized_vocabulary
     )
 
 
