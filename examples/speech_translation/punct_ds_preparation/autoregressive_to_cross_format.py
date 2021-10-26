d4a9fd5f0960a434b61675f5fec5ecd4c487c8e1
PeganovAnton <peganoff2@mail.ru>
2021-10-26 22:18:08 +0300

    Make minor improvements
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/autoregressive_to_cross_format.py
+++ examples/speech_translation/punct_ds_preparation/autoregressive_to_cross_format.py
@@ -76,7 +76,9 @@ def autoregressive_file_to_cross_format_file(
     if normalized_vocabulary and not_normalized_vocab_file is not None:
         with not_normalized_vocab_file.open('w') as f:
             json.dump(vocabulary, f, indent=2)
-    inverse_vocabulary = {v['string']: k for k, v in vocabulary.items()}
+    inverse_vocabulary = {
+        v['string']: k for k, v in (normalized_vocabulary if normalize_vocabulary else vocabulary.items())
+    }
     with input_file.open() as in_f, output_file.open('w') as out_f:
         for line in tqdm(in_f, total=num_lines, unit='line', desc="Encoding"):
             if normalized_vocabulary:
