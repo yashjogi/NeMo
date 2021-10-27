976875e44de4a148fcf6f3de9fb1d37a90a731b7
PeganovAnton <peganoff2@mail.ru>
2021-10-27 09:52:15 +0300

    Update command for punctuation inference
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/pretrained_bert_predict_labels_for_test.sh
+++ examples/nlp/token_classification/pretrained_bert_predict_labels_for_test.sh
@@ -1,8 +1,8 @@
 python punctuate_capitalize_infer.py \
   --input_text ~/NeMo/examples/nlp/token_classification/data/wmt/test/text_test.txt \
-  --output_text ~/NeMo/examples/nlp/token_classification/data/predictions_wmt/evelina_model_job2312172/pred.txt \
-  --model_path '~/NeMo/examples/nlp/token_classification/ngc_results/wmt/min_punc/2312172_lr1e-4_steps300000_gpu1/final.nemo' \
-  --max_seq_length 512 \
-  --step 510 \
-  --margin 0 \
+  --output_text ~/NeMo/examples/nlp/token_classification/data/predictions_wiki/evelina_model_with_quotes_draco_rno139405/pred.txt \
+  --model_path '~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_with_quotes_draco/checkpoints/Punctuation_and_Capitalization.nemo' \
+  --max_seq_length 64 \
+  --step 8 \
+  --margin 16 \
   --save_labels_instead_of_text
