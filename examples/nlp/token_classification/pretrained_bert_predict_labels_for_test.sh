1515feb98ecdf52eb3fcd2929052017dfbe75c9c
PeganovAnton <peganoff2@mail.ru>
2021-10-27 12:10:40 +0300

    Fix inference command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/pretrained_bert_predict_labels_for_test.sh
+++ examples/nlp/token_classification/pretrained_bert_predict_labels_for_test.sh
@@ -1,7 +1,7 @@
 python punctuate_capitalize_infer.py \
   --input_text ~/NeMo/examples/nlp/token_classification/data/wmt/test/text_test.txt \
   --output_text ~/NeMo/examples/nlp/token_classification/data/predictions_wiki/evelina_model_no_quotes_draco_rno139415/best/pred.txt \
-  --model_path '~/NWInf_results/autoregressive_punctuation_capitalization/evelina_model_no_quotes_draco_rno139415/checkpoints/Punctuation_and_Capitalization.nemo' \
+  --model_path '~/NWInf_results/autoregressive_punctuation_capitalization/evelina_model_no_quotes_draco_bert_base/checkpoints/Punctuation_and_Capitalization.nemo' \
   --max_seq_length 512 \
   --step 510 \
   --margin 0 \
