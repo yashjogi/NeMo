0e35ed4233d28b101dde8c90d1cb2d9936daf27f
PeganovAnton <peganoff2@mail.ru>
2021-10-27 15:52:02 +0300

    Add config for training locally on WMT
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/pretrained_bert_predict_labels_for_test.sh
+++ examples/nlp/token_classification/pretrained_bert_predict_labels_for_test.sh
@@ -6,3 +6,11 @@ python punctuate_capitalize_infer.py \
   --step 510 \
   --margin 0 \
   --save_labels_instead_of_text
+
+python punctuate_capitalize_infer.py \
+  --input_text ~/NeMo/examples/nlp/token_classification/data/wmt/test/text_test.txt \
+  --output_text ~/NeMo/examples/nlp/token_classification/data/predictions_wmt/train_on_wiki/evelina_model_with_quotes_draco_rno139405/best/pred_text.txt \
+  --model_path '~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_with_quotes_draco/checkpoints/Punctuation_and_Capitalization.nemo' \
+  --max_seq_length 512 \
+  --step 510 \
+  --margin 0
