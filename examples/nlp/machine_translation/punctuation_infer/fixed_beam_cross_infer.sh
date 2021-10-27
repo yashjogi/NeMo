d0ae7fd6d622927a8bb64928f53ad139bb1576ac
PeganovAnton <peganoff2@mail.ru>
2021-10-27 17:36:01 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/fixed_beam_cross_infer.sh
+++ examples/nlp/machine_translation/punctuation_infer/fixed_beam_cross_infer.sh
@@ -1,6 +1,6 @@
 ds_path=/home/apeganov/NeMo/examples/speech_translation/punct_prepared_datasets/prepared_punctuation_data_min_punctuation_27.10.2021_3.13
 output="${ds_path}/2370440_cross_labels_seq2seq_from_scratch_original_steps300000_gpu1/pred_cross_labels.txt"
-model_path="NeMo/examples/nlp/machine_translation/ngc_results/aayn_base_min_punc_cross/2370440_from_scratch_original_steps300000_gpu1/nemo_experiments/AAYNBase/2021-10-26_22-33-24/checkpoints/AAYNBase.nemo"
+model_path="ngc_results/aayn_base_min_punc_cross/2370440_from_scratch_original_steps300000_gpu1/nemo_experiments/AAYNBase/2021-10-26_22-33-24/checkpoints/AAYNBase.nemo"
 mkdir -p "$(dirname "${output}")"
 python nmt_transformer_infer.py \
     --model="${model_path}" \
