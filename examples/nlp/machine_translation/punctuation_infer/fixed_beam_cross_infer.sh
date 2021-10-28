a8ae2fb1b4b158c2c240f1a23acaf8a32a67b656
PeganovAnton <peganoff2@mail.ru>
2021-10-28 11:05:31 +0300

    Update cross labels inference script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/fixed_beam_cross_infer.sh
+++ examples/nlp/machine_translation/punctuation_infer/fixed_beam_cross_infer.sh
@@ -1,6 +1,6 @@
-ds_path=/home/apeganov/NeMo/examples/speech_translation/punct_prepared_datasets/prepared_punctuation_data_min_punctuation_27.10.2021_3.13
-output="${ds_path}/2370440_cross_labels_seq2seq_from_scratch_original_steps300000_gpu1/pred_cross_labels.txt"
-model_path="ngc_results/aayn_base_min_punc_cross/2370440_from_scratch_original_steps300000_gpu1/nemo_experiments/AAYNBase/2021-10-26_22-33-24/checkpoints/AAYNBase.nemo"
+ds_path=/home/apeganov/NeMo/examples/speech_translation/punct_prepared_datasets/prepared_punctuation_data_min_punctuation_28.10.2021_10.13
+output="${ds_path}/2371822_cross_labels_seq2seq_from_scratch_original_steps300000_gpu1/pred_cross_labels.txt"
+model_path="ngc_results/aayn_base_min_punc_cross/2371822_from_scratch_original_steps300000_gpu1/nemo_experiments/AAYNBase/2021-10-27_18-00-01/checkpoints/AAYNBase.nemo"
 mkdir -p "$(dirname "${output}")"
 python nmt_transformer_infer.py \
     --model="${model_path}" \
