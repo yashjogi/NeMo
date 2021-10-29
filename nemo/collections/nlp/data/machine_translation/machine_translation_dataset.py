6357177acdb2d15276bf5eeea9957835031ca9bc
PeganovAnton <peganoff2@mail.ru>
2021-10-29 19:19:44 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/data/machine_translation/machine_translation_dataset.py
+++ nemo/collections/nlp/data/machine_translation/machine_translation_dataset.py
@@ -236,6 +236,8 @@ class TranslationDataset(Dataset):
                     ]
                 tgt_mask = np.zeros_like(src_ids_, dtype=np.bool)
                 for i, sentence_idx in enumerate(b):
+                    print(f"tgt_word_mask[{sentence_idx}]:", tgt_word_mask[sentence_idx])
+                    print(f"tgt_mask.shape:", tgt_mask.shape)
                     tgt_mask[i, : len(tgt_word_mask[sentence_idx])] = tgt_word_mask[sentence_idx]
                 replacements = np.zeros_like(batches[batch_idx]["tgt_word_mask"], dtype=np.int32)
                 replacements[batches[batch_idx]["tgt_word_mask"]] = src_ids_[src_mask]
