7e93a439e0caf2c81f32a4cf9deedb7825f03ed4
PeganovAnton <peganoff2@mail.ru>
2021-11-02 02:53:09 +0300

    Add script for no tips launches
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/ngc_commands/train1_without_tips.sh
@@ -0,0 +1,27 @@
+WANDB_API_KEY="$1"
+
+read -r -d '' command << EOF
+set -e -x
+mkdir /result/nemo_experiments
+cd NeMo
+git pull
+git checkout decoder_with_tips
+bash reinstall.sh
+pip install pytorch-lightning==1.5.0rc1
+cd examples/nlp/machine_translation
+wandb login ${WANDB_API_KEY}
+python enc_dec_nmt.py \
+  --config-path=conf \
+  --config-name tips_aayn_base_min_punc_autoregressive_ngc \
+  trainer.gpus=1 \
+  model.use_decoder_tips=false
+set +e +x
+EOF
+
+ngc batch run \
+  --instance dgx1v.16g.1.norm \
+  --name "ml-model.aayn without_tips" \
+  --image "nvcr.io/nvidian/ac-aiapps/speech_translation:latest" \
+  --result /result \
+  --datasetid 90546:/data \
+  --commandline "${command}"
---
+++ examples/nlp/machine_translation/ngc_commands/train1_without_tips_sleep.sh
@@ -0,0 +1,23 @@
+WANDB_API_KEY="$1"
+
+read -r -d '' command << EOF
+set -e -x
+mkdir /result/nemo_experiments
+cd NeMo
+git pull
+git checkout decoder_with_tips
+bash reinstall.sh
+pip install pytorch-lightning==1.5.0rc1
+cd examples/nlp/machine_translation
+wandb login ${WANDB_API_KEY}
+sleep 200000
+set +e +x
+EOF
+
+ngc batch run \
+  --instance dgx1v.16g.1.norm \
+  --name "ml-model.aayn without_tips" \
+  --image "nvcr.io/nvidian/ac-aiapps/speech_translation:latest" \
+  --result /result \
+  --datasetid 90546:/data \
+  --commandline "${command}"
