3e8b9f4b19acd36d4480bd447d6781e8f796ad98
PeganovAnton <peganoff2@mail.ru>
2021-11-03 15:53:57 +0300

    Fix debug saving
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/nlp/models/machine_translation/mt_enc_dec_model.py
+++ nemo/collections/nlp/models/machine_translation/mt_enc_dec_model.py
@@ -408,11 +408,11 @@ class MTEncDecModel(EncDecNLPModel):
         np_tgt = tgt_ids.detach().cpu().numpy()
         ground_truths = [self.decoder_tokenizer.ids_to_text(tgt) for tgt in np_tgt]
         ground_truths = [self.target_processor.detokenize(tgt.split(' ')) for tgt in ground_truths]
-        with open('debug_translations.txt', 'a') as f, open('debug_references.txt', 'a'):
+        with open('debug_translations.txt', 'a') as tf, open('debug_references.txt', 'a') as rf:
             for tr in translations:
-                f.write(tr + '\n')
+                tf.write(tr + '\n')
             for gt in ground_truths:
-                f.write(gt + '\n')
+                rf.write(gt + '\n')
         num_non_pad_tokens = np.not_equal(np_tgt, self.decoder_tokenizer.pad_id).sum().item()
         tr_ctc, tr_lengths = self.encode_ctc(translations)
         gt_ctc, gt_lengths = self.encode_ctc(ground_truths)
