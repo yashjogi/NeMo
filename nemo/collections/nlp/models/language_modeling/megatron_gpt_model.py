7f64ae254b510c4b3571ae4d511fa71845477ac5
Sangkug Lym <slym@nvidia.com>
2021-11-17 19:04:20 -0800

    half precision training w/o autocast using master param
    stage fp16 working version
    
    fix: fp32 grad accumulation
    
    bf16 support
    
    Signed-off-by: Sangkug Lym <slym@nvidia.com>
    
    add closure fn at bf16

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -32,6 +32,7 @@ from nemo.collections.nlp.modules.common.megatron.megatron_init import (
     initialize_model_parallel_for_nemo,
     set_jit_fusion_options,
 )
+from nemo.collections.nlp.modules.common.megatron.module import Float16Module
 from nemo.collections.nlp.modules.common.megatron.utils import (
     average_losses_across_data_parallel_group,
     get_ltor_masks_and_position_ids,
@@ -106,6 +107,13 @@ class MegatronGPTModel(NLPModel):
             layernorm_epsilon=cfg.get('layernorm_epsilon', 1e-5),
             onnx_safe=cfg.get('onnx_safe', False),
         )
+
+        self.use_master_param = cfg.optim.get('master_param', False)
+        if self.use_master_param:
+            # pre-allocate the model on GPU to have master parameters allocated on the same device with matching data type
+            self.model.cuda(torch.cuda.current_device())
+            # Model wrapper to convert input to target half precision
+            self.model = Float16Module(self.model, cfg.precision)
 
     def forward(self, tokens, position_ids, attention_mask, labels):
         output_tensor = self.model(tokens, position_ids, attention_mask, labels=labels)
@@ -312,7 +320,11 @@ class MegatronGPTModel(NLPModel):
         if clip_val <= 0:
             return
 
-        parameters = self.model.parameters()
+        if self.use_master_param:
+            # grep fp32 master parameters for gradient clipping
+            parameters = self._optimizer.get_parameters()
+        else:
+            parameters = self.model.parameters()
         clip_grad_norm_fp32(parameters=parameters, max_norm=clip_val)
 
     def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> Any:
