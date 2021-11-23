d00ed2041c639450d34095f622b5efb92d4391e7
PeganovAnton <peganoff2@mail.ru>
2021-11-23 15:27:47 +0300

    Increase size of acquired dataset
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/ted_preparation.sh
+++ examples/speech_translation/punct_ds_preparation/ted_preparation.sh
@@ -8,5 +8,5 @@ python prepare_big_data_for_punctuation_capitalization_task_simple.py \
   --allowed_punctuation '.,?' \
   --only_first_punctuation_character_after_word_in_autoregressive \
   --no_label_if_all_characters_are_upper_case \
+  --size 150000 \
   --input_files ~/data/TED_Talks/en-ja/train.tags.en-ja.en
-  --num_jobs 24
