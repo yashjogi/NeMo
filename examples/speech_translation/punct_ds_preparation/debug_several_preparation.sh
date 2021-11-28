9c8ce76c98b0f290e4648dba508226cb6ee82cfe
PeganovAnton <peganoff2@mail.ru>
2021-11-28 17:24:08 +0300

    Use TED for debugging
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/debug_several_preparation.sh
+++ examples/speech_translation/punct_ds_preparation/debug_several_preparation.sh
@@ -1,6 +1,6 @@
 python prepare_big_data_for_punctuation_capitalization_task_simple.py \
   --output_dir /media/apeganov/DATA/debug_several_preparation \
-  --corpus_types europarl news-commentary \
+  --corpus_types TED \
   --create_model_input \
   --bert_labels \
   --autoregressive_labels \
@@ -8,6 +8,9 @@ python prepare_big_data_for_punctuation_capitalization_task_simple.py \
   --allowed_punctuation '.,?' \
   --only_first_punctuation_character_after_word_in_autoregressive \
   --no_label_if_all_characters_are_upper_case \
-  --input_files ~/data/europarl/v10/training-monolingual/europarl-v10.en.tsv \
-      ~/data/news-commentary/v16/training-monolingual/news-commentary-v16.en \
+  --input_files ~/data/TED_Talks/en-ja/train.tags.en-ja.en \
   --num_jobs 24
+
+#  --corpus_types europarl news-commentary \
+#  --input_files ~/data/europarl/v10/training-monolingual/europarl-v10.en.tsv \
+#      ~/data/news-commentary/v16/training-monolingual/news-commentary-v16.en \
