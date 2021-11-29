55d0e7878cef544ee462bd85640183ddc37e04d9
PeganovAnton <peganoff2@mail.ru>
2021-11-29 15:03:48 +0300

    Fix shuffling
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/shuffle_jointly.py
+++ examples/speech_translation/punct_ds_preparation/shuffle_jointly.py
@@ -1,7 +1,5 @@
 import argparse
-import io
 import os
-import tempfile
 from pathlib import Path
 from subprocess import run
 
@@ -25,6 +23,14 @@ def get_args() -> argparse.Namespace:
         required=True,
         help="It has to a character which does not occur in any of input files."
     )
+    parser.add_argument(
+        "--united_file_name",
+        default="united_lines.txt",
+    )
+    parser.add_argument(
+        "--shuffled_file_name",
+        default="shuffled_lines.txt",
+    )
     args = parser.parse_args()
     for i, f in enumerate(args.input_files):
         args.input_files[i] = f.expanduser()
@@ -37,22 +43,23 @@ def get_args() -> argparse.Namespace:
 
 def main():
     args = get_args()
-    united_fd, united_path = tempfile.mkstemp(text=True)
     input_file_objects = [inp_file.open() for inp_file in args.input_files]
+    united_file_path = args.input_files[0].parent / args.united_file_name
     lines = []
     for i, inp_obj in enumerate(input_file_objects):
         lines[i] = inp_obj.readline().strip('\n')
     line_number = 0
-    while all(lines):
-        for i, inp_obj in enumerate(input_file_objects):
-            lines[i] = inp_obj.readline()
-        delimiter_in_line = [args.line_delimiter in line for line in lines]
-        if any(delimiter_in_line):
-            raise ValueError(
-                f"Line delimiter {repr(args.line_delimiter)} is present in line number {line_number} in file "
-                f"{args.input_files[delimiter_in_line.index(True)]}."
-            )
-        os.write(united_fd, args.line_delimiter.join(lines) + '\n')
+    with united_file_path.open('w') as united_f:
+        while all(lines):
+            for i, inp_obj in enumerate(input_file_objects):
+                lines[i] = inp_obj.readline()
+            delimiter_in_line = [args.line_delimiter in line for line in lines]
+            if any(delimiter_in_line):
+                raise ValueError(
+                    f"Line delimiter {repr(args.line_delimiter)} is present in line number {line_number} in file "
+                    f"{args.input_files[delimiter_in_line.index(True)]}."
+                )
+            united_f.write(args.line_delimiter.join(lines) + '\n')
     if any(lines):
         raise ValueError(
             f"Files {', '.join([str(args.input_files[i]) for i, line in enumerate(lines) if not line])} "
@@ -60,22 +67,19 @@ def main():
         )
     for input_file_object in input_file_objects:
         input_file_object.close()
-    os.close(united_fd)
-    shuffled_fd, shuffled_path = tempfile.mkstemp(text=True)
-    shuffled_f = io.FileIO(shuffled_fd, mode='w', closefd=False)
-    run(['shuf', str(united_path)], stdout=shuffled_f)
-    os.remove(united_path)
-    shuffled_f.close()
+    shuffled_file_path = args.input_files[0].parent / args.shuffled_file_name
+    with shuffled_file_path.open('w') as f:
+        run(['shuf', str(united_file_path)], stdout=f)
+    os.remove(united_file_path)
     for out_file in args.output_files:
         out_file.parent.mkdir(parents=True, exist_ok=True)
     output_file_objects = [out_file.open('w') for out_file in args.output_files]
-    f = io.FileIO(shuffled_fd)
-    for tmp_line in f:
-        lines = tmp_line.strip().split(args.line_delimiter)
-        assert len(lines) == len(output_file_objects)
-        for i, line in enumerate(lines):
-            output_file_objects[i].write(line + '\n')
-    f.close()
+    with shuffled_file_path.open() as f:
+        for tmp_line in f:
+            lines = tmp_line.strip().split(args.line_delimiter)
+            assert len(lines) == len(output_file_objects)
+            for i, line in enumerate(lines):
+                output_file_objects[i].write(line + '\n')
     for output_file_object in output_file_objects:
         output_file_object.close()
 
