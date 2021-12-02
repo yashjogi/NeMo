8c6ebc227c529f4ae8db1cd9489d24a71fd1e58a
PeganovAnton <peganoff2@mail.ru>
2021-12-02 23:29:03 +0300

    Add sleep script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/token_classification/ngc_commands/ted/TEDx5_bs15000_steps50000_sleep.sh
@@ -0,0 +1,23 @@
+WANDB_API_KEY="$1"
+
+read -r -d '' command << EOF
+set -e -x
+OMP_NUM_THREADS=8
+cd NeMo
+git checkout feat/punc_tarred
+git pull
+bash reinstall.sh
+mkdir -p /result/nemo_experiments
+cd examples/nlp/token_classification
+wandb login ${WANDB_API_KEY}
+sleep 3600
+set +e +x
+EOF
+
+ngc batch run \
+  --instance dgx1v.16g.1.norm \
+  --name "ml-model.bert TED_punctuation_capitalization_" \
+  --image "nvcr.io/nvidian/ac-aiapps/speech_translation:latest" \
+  --result /result \
+  --datasetid 92254:/data \
+  --commandline "${command}"
