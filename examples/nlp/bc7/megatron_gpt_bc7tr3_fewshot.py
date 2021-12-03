7a01698fa42ebb10194ba436f3e5c5bf65b50759
Hoo Chang Shin <hshin@nvidia.com>
2021-12-02 20:11:37 -0500

    modified eval criteria.

--- examples/nlp/bc7/megatron_gpt_bc7tr3_fewshot.py
+++ examples/nlp/bc7/megatron_gpt_bc7tr3_fewshot.py
@@ -1,4 +1,5 @@
-import requests, json, random
+from collections import defaultdict
+import requests, json, random, csv
 
 def run(model, num_samples, tokens_to_generate, temperature):
     if model == 'nemo_1.3b':
@@ -46,9 +47,6 @@ def run(model, num_samples, tokens_to_generate, temperature):
             val_pred.append(response_ans)
             # print(response.text.encode('utf-8'))
 
-        if _ > 2:
-            break
-
     tp, fp, fn = 0, 0, 0
     for si in range(len(val_true)):
         true_val = val_true[si]
@@ -57,6 +55,13 @@ def run(model, num_samples, tokens_to_generate, temperature):
         pred_vals = list(set(pred_val.split(',')))
         true_vals = list(set(true_val.split(',')))
         
+        if true_vals == ['{none}']:
+            if pred_vals == ['{none}'] or pred_vals == ['']:
+                tp += 1
+            else:
+                fp += len(pred_vals)     
+            continue
+
         for pred_vali in pred_vals:
             if pred_vali in true_vals:
                 tp += 1
@@ -76,10 +81,17 @@ def run(model, num_samples, tokens_to_generate, temperature):
 
 if __name__ == "__main__":
 
-    NUM_PROMPT_SAMPLES = 10
-    TOKENS_TO_GENERATE = 5
-    TEMPERATURE = 0.1
-
-    # run('nemo_1.3b')
-    prec, rec, f1 = run('megatron_530b', NUM_PROMPT_SAMPLES, TOKENS_TO_GENERATE, TEMPERATURE)
-    
+    # NUM_PROMPT_SAMPLES = 10
+    # TOKENS_TO_GENERATE = 5
+    # TEMPERATURE = 0.1
+    with open('gpt_fewshot_bc7tr3_evals.csv', 'w') as csvfw:
+        csvw = csv.writer(csvfw, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvw.writerow(['model', 'NUM_PROMPT_SAMPLES', 'TOKENS_TO_GENERATE', 'TEMPERATURE', 'prec', 'rec', 'f1'])
+        hyper_params = [(5, 5, 0.1), (10, 5, 0.1), (5, 10, 0.1), (10, 10, 0.1), (5, 5, 0.3), (10, 10, 0.3)]
+        models = ['megatron_530b']#, 'nemo_1.3b']
+        for modeli in models:
+            for hpi in hyper_params:
+                NUM_PROMPT_SAMPLES, TOKENS_TO_GENERATE, TEMPERATURE = hpi
+                prec, rec, f1 = run('megatron_530b', NUM_PROMPT_SAMPLES, TOKENS_TO_GENERATE, TEMPERATURE)
+                csvw.writerow([modeli, NUM_PROMPT_SAMPLES, TOKENS_TO_GENERATE, TEMPERATURE, prec, rec, f1])
+        
