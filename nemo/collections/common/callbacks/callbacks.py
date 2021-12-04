99ae59ab245aa0dc2448654e94c2c8c4642b3870
PeganovAnton <peganoff2@mail.ru>
2021-12-04 10:41:21 +0300

    backup
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- nemo/collections/common/callbacks/callbacks.py
+++ nemo/collections/common/callbacks/callbacks.py
@@ -12,11 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import time
+from typing import List
 
 import numpy as np
+import torch
+from hydra.utils import instantiate
+from omegaconf import DictConfig
 from pytorch_lightning.callbacks.base import Callback
 from pytorch_lightning.utilities import rank_zero_only
 
+from pytorch_lightning import Trainer, LightningModule
 # from sacrebleu import corpus_bleu
 
 
@@ -35,63 +40,16 @@ class LogEpochTimeCallback(Callback):
         trainer.logger.log_metrics({"epoch_time": duration}, step=trainer.global_step)
 
 
-# class MachineTranslationLogEvalCallback(Callback):
-#     def _on_eval_end(self, trainer, pl_module, mode):
-#         counts = np.array(self._non_pad_tokens)
-#         eval_loss = np.sum(np.array(self._losses) * counts) / np.sum(counts)
-#         sacre_bleu = corpus_bleu(self._translations, [self._ground_truths], tokenize="13a")
-#         print(f"{mode} results for process with global rank {pl_module.global_rank}".upper())
-#         for i in range(pl_module.num_examples[mode]):
-#             print('\u0332'.join(f"EXAMPLE {i}:"))  # Underline output
-#             sent_id = np.random.randint(len(self._translations))
-#             print(f"Ground truth: {self._ground_truths[sent_id]}\n")
-#             print(f"Translation: {self._translations[sent_id]}\n")
-#             print()
-#         print("-" * 50)
-#         print(f"loss: {eval_loss:.3f}")
-#         print(f"SacreBLEU: {sacre_bleu}")
-#         print("-" * 50)
+class ResetOptimizerStepsCallback(Callback):
+    def on_before_optimizer_step(
+        self, trainer: Trainer, pl_module: LightningModule, optimizer: torch.optim.Optimizer, opt_idx: int
+    ) -> None:
+        if pl_module.global_step > 0 and pl_module.global_step % pl_module.optimizer_reset_period == 0:
+            optimizer.load_state_dict(pl_module.optimizer_reset_state_dict)
 
-#     @rank_zero_only
-#     def on_test_end(self, trainer, pl_module):
-#         self._on_eval_end(trainer, pl_module, "test")
 
-#     @rank_zero_only
-#     def on_validation_end(self, trainer, pl_module):
-#         self._on_eval_end(trainer, pl_module, "val")
-
-#     @rank_zero_only
-#     def on_sanity_check_end(self, trainer, pl_module):
-#         self._on_eval_end(trainer, pl_module, "val")
-
-#     def _on_eval_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx, mode):
-#         self._translations.extend(outputs['translations'])
-#         self._ground_truths.extend(outputs['ground_truths'])
-#         self._non_pad_tokens.append(outputs['num_non_pad_tokens'])
-#         self._losses.append(outputs[f'{mode}_loss'])
-
-#     @rank_zero_only
-#     def on_test_batch_end(self, trainer, pl_module, batch, outputs, batch_idx, dataloader_idx):
-#         self._on_eval_batch_end(trainer, pl_module, batch, outputs, batch_idx, dataloader_idx, 'test')
-
-#     @rank_zero_only
-#     def on_validation_batch_end(self, trainer, pl_module, batch, outputs, batch_idx, dataloader_idx):
-#         self._on_eval_batch_end(trainer, pl_module, batch, outputs, batch_idx, dataloader_idx, 'val')
-
-#     def _on_eval_start(self, trainer, pl_module):
-#         self._translations = []
-#         self._ground_truths = []
-#         self._losses = []
-#         self._non_pad_tokens = []
-
-#     @rank_zero_only
-#     def on_test_start(self, trainer, pl_module):
-#         self._on_eval_start(trainer, pl_module)
-
-#     @rank_zero_only
-#     def on_validation_start(self, trainer, pl_module):
-#         self._on_eval_start(trainer, pl_module)
-
-#     @rank_zero_only
-#     def on_sanity_check_start(self, trainer, pl_module):
-#         self._on_eval_start(trainer, pl_module)
+def instantiate_callbacks(callback_configs: List[DictConfig]) -> List[Callback]:
+    callbacks = []
+    for conf in callback_configs:
+        callbacks.append(instantiate(conf))
+    return callbacks
