d68a6b411b82f1342fcbed00075c64e19413aab7
PeganovAnton <peganoff2@mail.ru>
2021-12-04 12:50:32 +0300

    fix path to model
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/test_seq_len_step_margin_wiki_plus_wmt_with_quotes_large.sh
+++ examples/nlp/token_classification/test_seq_len_step_margin_wiki_plus_wmt_with_quotes_large.sh
@@ -1,5 +1,5 @@
 python test_seq_len_step_margin.py \
-  --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_large/results_iwslt_tst2019_punctuation_and_capitalization_BERT_large_lr6e-5_warmupratio0.06_steps100k_wasrestore_4.12.2021.nemo \
+  --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_large/punctuation_and_capitalization_BERT_large_lr6e-5_warmupratio0.06_steps100k_wasrestore_4.12.2021.nemo \
   --labels ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/labels_iwslt_en_text.txt \
   --source_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
   --output_dir ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_large/param_selection_for_punctuation_punctuation_and_capitalization_BERT_large_lr6e-5_warmupratio0.06_steps100k_wasrestore_4.12.2021 \
