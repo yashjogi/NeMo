fb05ad964432dbca34bee8733d71ec342b9f022f
PeganovAnton <peganoff2@mail.ru>
2021-12-05 16:44:00 +0300

    Add more prints to slurm script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/slurm_runner.sh
+++ examples/slurm_runner.sh
@@ -5,6 +5,7 @@ results_dir="$2"
 
 output=$(eval ${sbatch_command})
 job_id=$(echo ${output} | grep -oP "\d+")
+echo "Current job id: ${job_id}"
 
 sleep 5
 
@@ -15,8 +16,10 @@ done
 path_to_errors="$(find "${results_dir}" -name "error-${job_id}-0.out")"
 
 while [ ! -z "$(grep 'DUE TO TIME LIMIT ***' "${path_to_errors}")" ]; do
+  echo RESTARTING
   output=$(eval ${sbatch_command})
   job_id=$(echo ${output} | grep -oP "\d+")
+  echo "Current job id: ${job_id}"
 
   sleep 5
 
