2914b0a3eec15c8c62575fb65d12025d7e0eb4ca
PeganovAnton <peganoff2@mail.ru>
2021-12-06 17:30:01 +0300

    Add base slurm script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/slurm_commands/train_wiki_wmt_base.sh
+++ examples/nlp/token_classification/slurm_commands/train_wiki_wmt_base.sh
@@ -6,7 +6,7 @@
 #SBATCH --exclusive
 #SBATCH --mem=0
 #SBATCH --gpus-per-node=16
-#SBATCH -J "ent_aiapps_asr:punctuation_capitalization_using_NMT_large24x6_bs18000"  # job name (<< CHANGE ! >>)
+#SBATCH -J "ent_aiapps_asr:punctuation_capitalization_on_wiki_with_quotes"  # job name (<< CHANGE ! >>)
 #SBATCH --mail-type=FAIL        # only send email on failure
 #SBATCH --overcommit
 #SBATCH --ntasks-per-node=16     # n tasks per machine (one task per gpu) <required>
@@ -19,13 +19,13 @@ WANDB="${wandb}" # replace with your own WandB API key
 # total_tokens = max_steps * global_batch_size_in_tokens
 # global_batch_size_in_tokens = micro_batch_size * data_parallel_size * accumulate_grad_batches * seq_length
 # data_parallel_size = num_nodes * num_gpus_per_node (no model parallel)
-MAX_STEPS=400000
+MAX_STEPS=150000
 VAL_CHECK_INTERVAL=2000
 LOG_EVERY_N_STEPS=100
 
 # Logging
 PROJECT="autoregressive_punctuation_capitalization"
-EXPNAME="nmt_wiki_wmt_large24x6_bs204000_steps400000_lr3e-4_cross"
+EXPNAME="evelina_wiki_wmt_base_lr1e-4_steps150000"
 
 # Mounts
 SLURM_ACCOUNT='ent_aiapps'
@@ -52,22 +52,19 @@ echo "*******STARTING********" \
 && echo "Starting training" \
 && cd /code/ \
 && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 python \
-  /code/examples/nlp/machine_translation/enc_dec_nmt.py \
-	--config-path=/code/examples/nlp/machine_translation/conf/wiki_wmt \
-	--config-name=local_large24x6_tarred13000_cross \
-	model.tgt_character_vocabulary="/data/cross_char_vocabulary.txt" \
-	model.train_ds.use_tarred_dataset=true \
-	model.train_ds.metadata_file="/data/train_cross_tarred_13000/metadata.tokens.13000.json" \
-	model.validation_ds.src_file_name="/data/IWSLT_tst2019/input.txt" \
-	model.validation_ds.tgt_file_name="/data/IWSLT_tst2019/cross_labels.txt" \
-	model.test_ds.src_file_name="/data/IWSLT_tst2019/input.txt" \
-	model.test_ds.tgt_file_name="/data/IWSLT_tst2019/cross_labels.txt" \
-	model.encoder_tokenizer.tokenizer_model="/data/input.BPE.25000.model" \
-	model.decoder_tokenizer.vocab_file="/data/cross_char_vocabulary.txt" \
-	trainer.num_nodes=${SLURM_JOB_NUM_NODES} \
+  /code/examples/nlp/token_classification/punctuation_capitalization_train_evaluate.py \
+	--config-path=/code/examples/nlp/token_classification/conf/wiki \
+	--config-name=local_bs15000_steps100000 \
+	do_testing=false \
+	model.train_ds.ds_item="/data/train_bert_tarred_10000" \
+	model.train_ds.tar_metadata_file="metadata.punctuation_capitalization.tokens10000.max_seq_length512.bert-large-uncased.json" \
+	model.validation_ds.ds_item=[/data/europarl_dev,/data/news_commentary_dev,/data/rapid_dev,/data/wiki_dev,/data/IWSLT_tst2019] \
+	model.validation_ds.tokens_in_batch=4092 \
+	~model.test_ds \
+	+trainer.num_nodes=${SLURM_JOB_NUM_NODES} \
 	trainer.gpus=${SLURM_NTASKS_PER_NODE} \
 	trainer.max_steps=${MAX_STEPS} \
-	trainer.val_check_interval=${VAL_CHECK_INTERVAL} \
+	+trainer.val_check_interval=${VAL_CHECK_INTERVAL} \
 	exp_manager.create_wandb_logger=true \
 	exp_manager.wandb_logger_kwargs.name=${EXPNAME} \
 	exp_manager.wandb_logger_kwargs.project=${PROJECT} \
@@ -75,12 +72,12 @@ echo "*******STARTING********" \
 	+exp_manager.resume_if_exists=True \
 	+exp_manager.resume_ignore_no_checkpoint=True \
 	exp_manager.create_checkpoint_callback=True \
-	exp_manager.checkpoint_callback_params.monitor=val_CER \
 	+exp_manager.checkpoint_callback_params.save_top_k=3 \
-	exp_manager.checkpoint_callback_params.mode=min \
+	exp_manager.checkpoint_callback_params.monitor=IWSLT_tst2019_val_punct_f1 \
+	exp_manager.checkpoint_callback_params.mode=max \
 	+exp_manager.checkpoint_callback_params.always_save_nemo=False \
-	model.optim.lr=3e-4 \
-	model.optim.sched.warmup_ratio=0.1
+	model.optim.lr=1e-4 \
+	model.optim.sched.warmup_ratio=0.07
 EOF
 
 srun -o $OUTFILE -e $ERRFILE --container-image="$CONTAINER" $MOUNTS bash -c "${cmd}"
