732c52bfc403725e646662ce601daa09ec873e72
PeganovAnton <peganoff2@mail.ru>
2021-12-09 10:17:36 +0300

    Increase number of steps for base NMT models
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/slurm_commands/train_wiki_wmt_base.sh
+++ examples/nlp/machine_translation/slurm_commands/train_wiki_wmt_base.sh
@@ -19,13 +19,13 @@ WANDB="${wandb}" # replace with your own WandB API key
 # total_tokens = max_steps * global_batch_size_in_tokens
 # global_batch_size_in_tokens = micro_batch_size * data_parallel_size * accumulate_grad_batches * seq_length
 # data_parallel_size = num_nodes * num_gpus_per_node (no model parallel)
-MAX_STEPS=200000
+MAX_STEPS=400000
 VAL_CHECK_INTERVAL=2000
 LOG_EVERY_N_STEPS=100
 
 # Logging
 PROJECT="autoregressive_punctuation_capitalization"
-EXPNAME="nmt_wiki_wmt_base_bs400000_steps200000_lr2e-4"
+EXPNAME="nmt_wiki_wmt_base_bs400000_steps400000_lr2e-4"
 
 # Mounts
 SLURM_ACCOUNT='ent_aiapps'
