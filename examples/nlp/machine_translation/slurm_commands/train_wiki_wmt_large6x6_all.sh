5b57ed378bf354c2e7c530ffd40cea29d5acfe04
PeganovAnton <peganoff2@mail.ru>
2021-12-13 17:58:56 +0300

    Fix word tokens
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/slurm_commands/train_wiki_wmt_large6x6_all.sh
+++ examples/nlp/machine_translation/slurm_commands/train_wiki_wmt_large6x6_all.sh
@@ -64,6 +64,7 @@ echo "*******STARTING********" \
 	model.test_ds.tgt_file_name="/data/IWSLT_tst2019/autoregressive_labels.txt" \
 	model.encoder_tokenizer.tokenizer_model="/data/input.BPE.25000.model" \
 	model.decoder_tokenizer.tokenizer_model="/data/autoregressive_labels.BPE.18.model" \
+	model.decoder_tokenizer.word_tokens=[u,O,U] \
 	trainer.num_nodes=${SLURM_JOB_NUM_NODES} \
 	trainer.gpus=${SLURM_NTASKS_PER_NODE} \
 	trainer.max_steps=${MAX_STEPS} \
