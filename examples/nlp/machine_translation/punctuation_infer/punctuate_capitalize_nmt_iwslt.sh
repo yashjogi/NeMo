f8580c13df04b0edcbfbd1bce2c151be2b4e5388
PeganovAnton <peganoff2@mail.ru>
2021-12-13 18:11:08 +0300

    Add combat solution for punctuation and capitalization
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/punctuate_capitalize_nmt_iwslt.sh
+++ examples/nlp/machine_translation/punctuation_infer/punctuate_capitalize_nmt_iwslt.sh
@@ -1,6 +1,6 @@
 python punctuate_capitalize_nmt.py \
-    --input_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/one_line_text_iwslt_en_text.txt \
-    --output_text debug_punctuate_capitalize_nmt.txt \
+    --input_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
+    --output_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/nmt/wiki_wmt_large6x6_bs400000_steps400000_lr2e-4.txt \
     --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/nmt_wiki_wmt_large6x6_bs400000_steps400000_lr2e-4/checkpoints/AAYNLarge6x6.nemo \
     --max_seq_length 128 \
     --step 8 \
