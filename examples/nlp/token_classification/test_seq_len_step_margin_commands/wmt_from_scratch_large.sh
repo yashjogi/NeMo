6a4eee3f847a36299a0504f7e44b867151680820
PeganovAnton <peganoff2@mail.ru>
2021-12-17 12:50:08 +0300

    Make minor fix
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/test_seq_len_step_margin_commands/wmt_from_scratch_large.sh
+++ examples/nlp/token_classification/test_seq_len_step_margin_commands/wmt_from_scratch_large.sh
@@ -1,5 +1,5 @@
 python test_seq_len_step_margin.py \
-  --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wmt_base_lr1e-4_steps150k_from_scratch_bs160k/checkpoints/Punctuation_and_Capitalization.nemo \
+  --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wmt_large_lr6e-5_steps300k_from_scratch_bs160k/checkpoints/Punctuation_and_Capitalization.nemo \
   --labels ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/labels_iwslt_en_text.txt \
   --source_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
   --output_dir ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/evelina_model_large_from_scratch_wmt \
