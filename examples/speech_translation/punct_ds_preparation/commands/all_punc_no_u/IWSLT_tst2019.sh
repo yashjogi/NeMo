5a3588677e29d97e72bac441d932e57c5ec115db
PeganovAnton <peganoff2@mail.ru>
2021-12-17 20:37:18 +0300

    Add scripts for training all_punc_no_u
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/commands/all_punc_no_u/IWSLT_tst2019.sh
+++ examples/speech_translation/punct_ds_preparation/commands/all_punc_no_u/IWSLT_tst2019.sh
@@ -1,6 +1,6 @@
 python text_to_punc_cap_dataset.py \
   --input_text /media/apeganov/DATA/punctuation_and_capitalization/simplest/wiki_wmt_92_128_29.11.2021/for_upload/IWSLT_tst2019/text.txt \
-  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/all_punc_u/wiki_wmt_92_128_12.12.2021/for_upload/IWSLT_tst2019 \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/all_punc_no_u/wiki_wmt_92_128_12.12.2021/for_upload/IWSLT_tst2019 \
   --create_model_input \
   --bert_labels \
   --autoregressive_labels \
