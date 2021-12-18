10dbb9360cb85df73c7403595a3bd432ff09b492
PeganovAnton <peganoff2@mail.ru>
2021-12-18 17:02:57 +0300

    Increase batch size
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/commands/punctuate_capitalize_nmt_iwslt_trained_on_wmt_large6x6_no_overlapping.sh
+++ examples/nlp/machine_translation/punctuation_infer/commands/punctuate_capitalize_nmt_iwslt_trained_on_wmt_large6x6_no_overlapping.sh
@@ -10,7 +10,7 @@ python punctuate_capitalize_nmt.py \
     --max_seq_length 64 \
     --step 32 \
     --margin 16 \
-    --batch_size 36 \
+    --batch_size 80 \
     --no_all_upper_label \
     --add_source_num_words_to_batch \
     --make_queries_contain_intact_sentences
