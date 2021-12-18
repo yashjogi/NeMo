b12f616d20cc31a163f0fbe5617d0ca3c854a44a
PeganovAnton <peganoff2@mail.ru>
2021-12-18 11:24:50 +0300

    Add scripts for scoring
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/machine_translation/punctuation_infer/commands/fixed_beam_search_from_draco_trained_on_wmt_large6x6.sh
@@ -0,0 +1,20 @@
+ds_path=/media/apeganov/DATA/punctuation_and_capitalization/simplest/wmt_92_128_29.11.2021
+output="${ds_path}/preds/nmt_wmt_large6x6_bs400000_steps300000_lr2e-4/preds.txt"
+model_path="/home/apeganov/NWInf_results/autoregressive_punctuation_capitalization/nmt_wmt_large6x6_bs400000_steps300000_lr2e-4/checkpoints/AAYNLarge6x6.nemo"
+mkdir -p "$(dirname "${output}")"
+python nmt_transformer_infer.py \
+    --model=${model_path} \
+    --srctext="${ds_path}/for_upload/IWSLT_tst2019/input.txt" \
+    --tgtout="${output}" \
+    --word_tokens u U O \
+    --add_src_num_words_to_batch \
+    --cuda_device 1
+
+cd punctuation_infer
+python compute_metrics.py \
+    --hyp ${output} \
+    --ref "${ds_path}/for_upload/IWSLT_tst2019/autoregressive_labels.txt" \
+    --output "${ds_path}/preds/nmt_wmt_large6x6_bs400000_steps300000_lr2e-4/scores.json" \
+    --normalize_punctuation_in_hyp
+cd -
+set +e
