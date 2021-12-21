e93b06c7db1adef969116ad8930f93f002bc2a07
PeganovAnton <peganoff2@mail.ru>
2021-12-21 19:56:19 +0300

    Add more scripts for launching and data preparation
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/commands/simplest/3_128/europarl.sh
@@ -0,0 +1,15 @@
+python prepare_big_data_for_punctuation_capitalization_task_simple.py \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/europarl_x3_92_128_29.11.2021 \
+  --corpus_types europarl \
+  --create_model_input \
+  --bert_labels \
+  --autoregressive_labels \
+  --sequence_length_range 3 128 \
+  --allowed_punctuation '.,?' \
+  --only_first_punctuation_character_after_word_in_autoregressive \
+  --no_label_if_all_characters_are_upper_case \
+  --input_files ~/data/europarl/v10/training-monolingual/europarl-v10.en.tsv \
+  --num_jobs 24 \
+  --num_passes_through_dataset 3 \
+  --dev_size 10000 \
+  --test_size 0
